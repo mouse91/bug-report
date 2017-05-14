8d31edaece4de343608b8131ff54c550df77dda1
mouse91 <mouse91@users.noreply.github.com>
2017-05-14 19:39:15 +0100

    Initial commit

---
+++ bugreport.js
@@ -0,0 +1,53 @@
+function updateSyntax() {
+    var desc = $('#desc-field').val();
+    var expected = $('#exp-field').val();
+    var actual = $('#act-field').val();
+    var settings = $('#sys-field').val();
+    var steps = '';
+    var bugtext = '';
+    for (var i = 1; i <= window.sct; i++) {
+        var step = $('#s' + i + '-field').val();
+        if (step) {
+            steps = steps + ' - ' + step;
+        }
+    }
+    if (desc && expected && actual && steps) {
+        bugtext = '!submit ' + desc + ' | Steps to Reproduce:' + steps + ' Expected Result: ' + expected + ' Actual Result: ' + actual;
+        if (settings) {
+            bugtext = bugtext + ' System Settings: ' + settings;
+        }
+    }
+    $('#syntax').text(bugtext);
+}
+
+function addStep() {
+    window.sct++;
+    var stxt = '<div class="input-group" id="s' + window.sct + '-grp"><span class="input-group-label"><small>Step ' + window.sct + '</small></span><input type="text" class="input-group-field" id="s' + window.sct + '-field"></div>';
+    $('#steps-fs').append(stxt);
+}
+
+function removeStep() {
+    if (window.sct > 1) {
+        $('#s' + window.sct + '-grp').remove();
+        window.sct--;
+        updateSyntax();
+    }
+}
+
+$(document).ready(function() {
+    window.sct = 1;
+    $(document).on('input', 'input[id*="-field"]', updateSyntax);
+    $('#add-btn').on('click', addStep);
+    $('#del-btn').on('click', removeStep);
+    var cb = new Clipboard('#copy-btn', {
+        text: function(trigger) {
+            return $('#syntax').text();
+        }
+    });
+    cb.on('success', function(e) {
+        $(e.trigger).html('Copied');
+        setTimeout(function() {
+            $(e.trigger).html('Copy');
+        }, 2000);
+    });
+});
